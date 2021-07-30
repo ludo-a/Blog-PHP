8282a0077d15cfa88d8bbdeb39264a8a1c107be8
ludovic adach <adachludovic@gmail.com>
2021-07-30 09:46:37 +0200

    résolution d'un bug lors de la mise en place de la condition pour vérifier si le numéro de billet existe dans la BDD

--- commentaires.php
+++ commentaires.php
@@ -9,21 +9,45 @@ require('connexionBDD.php');
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>Document</title>
+    <title>Blog</title>
 </head>
 <body>
-    <?php include("menu.php") ?>
-    <h1>Blog</h1>
-    <p class="lastBill">Les Commentaires de : <?php echo $_GET['titre']; ?></p>
 
+<?php include("menu.php") ?>
+
+<h1>Blog</h1>
+    
 <?php 
-
-$req = $bdd->prepare("SELECT auteur, commentaire, date_commentaire, DATE_FORMAT(date_commentaire, '%e %b %Y à %H:%i') AS date_commentaire_fr FROM commentaires WHERE id_billet = ? ORDER BY date_commentaire");
+//Verification en cas de modification de l'url
+$req = $bdd->prepare("SELECT id_billet FROM commentaires WHERE id_billet = ?");
 $req->execute(array($_GET['billet']));
+$condition = $req->fetch();
+if($condition == false){
+//var_dump($condition); //Affichage valeur de retour DEBUG MODE
+?>
+<div class="textDesc">
+    <h3>
+        OUPS !!!
+    </h3>
+    <p class="contenu-text">
+        Il semblerait que le contenu n'existe pas.</br>
+        Vous êtes prié(e) de ne pas modifier l'URL, Merci.
+    </p>
+</div>
 
-while($donnees = $req->fetch())
+<?php
+}
+else{ // sinon affichage de la BDD
+    $req2 = $bdd->prepare("SELECT auteur, commentaire, date_commentaire, DATE_FORMAT(date_commentaire, '%e %b %Y à %H:%i') AS date_commentaire_fr FROM commentaires WHERE id_billet = ? ORDER BY date_commentaire");
+    $req2->execute(array($_GET['billet']));
+?>
+
+<p class="lastBill">Les Commentaires de : <?php //echo $_GET['titre']; ?></p>
+
+<?php while($donnees = $req2->fetch())
 {
 ?>
+
 <div class="textDesc">
     <h3>
         <?php echo htmlspecialchars($donnees['auteur']); ?>
@@ -33,23 +57,26 @@ while($donnees = $req->fetch())
         <?php echo nl2br(htmlspecialchars($donnees['commentaire'])); ?>
     </p>
 </div>
-<?php
-}
-$req->closeCursor();
+
+<?php }
+$req2->closeCursor(); 
 ?>
+
+<!-- Formulaire d'ajout de commentaire -->
 <div class="textDesc">
     <h3>Ajouter un commentaire</h3>
     <form method="post" action="commentaire_post.php">
         <label>Nom ou Pseudo</label></br>
         <input type="text" name="auteur"></br>
         <input class="nonvisible" type="text" name="id_billet" value="<?php echo $_GET['billet'] ?>" readonly></br>
-        
         <label>Commentaire</label></br>
-        <textarea name="commentaire" id="" cols="45" rows="5"></textarea>
-        
+        <textarea name="commentaire" id="" cols="45" rows="5"></textarea> 
         <input class="nonvisible" type="text" name="titre" value="<?php echo $_GET['titre'] ?>" readonly></br>
         <button type="submit">Envoyer</button>
     </form>
 </div>
+
+<?php } ?>
+
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,6 +1,4 @@
-<?php 
-require('connexionBDD.php');
-?>
+<?php require('connexionBDD.php'); ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -9,19 +7,19 @@ require('connexionBDD.php');
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>Document</title>
+    <title>Blog</title>
 </head>
 <body>
-    <?php include("menu.php") ?>
-    <h1>Blog</h1>
-    <p class="lastBill">Derniers billets</p>
+
+<?php include("menu.php") ?>
+
+<h1>Blog</h1>
+<p class="lastBill">Derniers billets</p>
     
 <?php 
-
 $req = $bdd->query("SELECT id, titre, contenu, date_creation, DATE_FORMAT(date_creation, '%e %b %Y à %H:%i') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 5");
-while($donnees = $req->fetch())
-{
-?>
+while($donnees = $req->fetch()){ ?>
+
 <div class="textDesc">
     <h3>
         <?php echo htmlspecialchars($donnees['titre']); ?><br/>
@@ -33,9 +31,9 @@ while($donnees = $req->fetch())
         <em><a href="commentaires.php?billet=<?php echo $donnees['id']; ?>&titre=<?php echo $donnees['titre']; ?>">Commentaires</a></em>
     </p>
 </div>
-<?php
-}
-$req->closeCursor();
-?>
+
+<?php }
+$req->closeCursor(); ?>
+
 </body>
 </html>
